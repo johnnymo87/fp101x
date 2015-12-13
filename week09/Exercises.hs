ec141f19edd361ba63f291858924e746cc196563
johnnymo87 <johnnymo87@gmail.com>
2015-12-13 19:54:08 +0000

    Implement integerToNat

--- week09/Exercises.hs
+++ week09/Exercises.hs
@@ -4,10 +4,13 @@ import Data.Char
 -- Q: Why do we need to import unsafeCoerce in exercise 0? It is not used in any possible answer option.
 -- A: My guess is, it's to be able to use a common supplied-code snippet for Exercise 0 and Exercise 1.
 -- import Hugs.IOExts (unsafeCoerce)
+import Unsafe.Coerce (unsafeCoerce)
 
 data Nat = Zero
          | Succ Nat
-         deriving Show
+         deriving (Show, Eq)
+         -- Show: to support the meta-programming defining of natToInteger
+         -- Eq: to support `shouldBe` for integerToNat
 
 natToInteger :: Nat -> Integer
 -- natToInteger Zero = 0
@@ -20,3 +23,7 @@ natToInteger :: Nat -> Integer
 
 -- meta-programming?
 natToInteger n = genericLength [c | c <- show n, c == 'S']
+
+integerToNat :: Integer -> Nat
+integerToNat 0 = Zero
+integerToNat n = Succ (integerToNat (n - 1))
--- week09/ExercisesSpec.hs
+++ week09/ExercisesSpec.hs
@@ -12,3 +12,8 @@ main = hspec $ do
     it "converts any non-bottom, non-partial, finite natural number (note: 0 is a natural number according to this definition), into the corresponding Integer value" $ do
       natToInteger Zero `shouldBe` 0
       natToInteger (Succ (Succ Zero)) `shouldBe` 2
+
+  describe "integerToNat" $ do
+    it "converts any non-bottom, non-partial, finite Integer value >= 0, into the corresponding Nat value" $ do
+      integerToNat 0 `shouldBe` Zero
+      integerToNat 2 `shouldBe` (Succ (Succ Zero))
