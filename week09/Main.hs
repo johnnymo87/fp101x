363cb5293ce2264fc8f9b4e0395716a177ebd9a4
johnnymo87 <johnnymo87@gmail.com>
2015-12-16 00:33:06 +0000

    Experiment with printing utf8 characters

---
+++ week09/Main.hs
@@ -0,0 +1,8 @@
+infixr 5 🚀
+
+-- @Complexion Programming is the space age work I do while listening to space age love songs 🚀🎶on #TFBS
+-- @Complexion Slaying 👾 in computer programs while I listen space age love songs 🚀🎶on #TFBS
+(🚀) = (:)
+(🌌) = []
+
+main = putStrLn $ 'T' 🚀 'F' 🚀 'B' 🚀 'S' 🚀 (🌌)
